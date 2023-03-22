99dbfdc6f852924bad3833c068c07d7893d2b642
Kristian Rosales <r.kristian40@gmail.com>
2023-03-22 15:14:38 -0500

    ready to start

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,13 @@
 // implement your posts router here
+const express = require("express");
+
+const Posts = require("./posts-model.js");
+
+const router = express.router();
+
+// router.get("/api/posts", (req, res) => {
+
+// })
+router.get("/", (req, res) => {
+  res.send("hello world");
+});
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
+});
+
+const port = 9000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
+module.exports = server;
